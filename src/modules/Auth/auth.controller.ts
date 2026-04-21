d5ab7efdb254b0f5b29721665c21507f4ece2968
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-04-21 22:10:43 +0600

    (add): implements register functionality

--- src/modules/Auth/auth.controller.ts
+++ src/modules/Auth/auth.controller.ts
@@ -1,3 +1,24 @@
+import { Request, Response } from "express";
+import { AuthService } from "./auth.service";
+
+const createUser = async (req: Request, res: Response) => {
+    try {
+        // console.log(req.body);
+        const result = await AuthService.createUser(req.body);
+        return res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            data: result,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: "User creation failed",
+            details: error,
+        });
+    }
+};
+
 export const AuthController = {
-    // Add controller methods here
-    };
+    createUser,
+};
--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,3 +1,23 @@
+import bcrypt from "bcryptjs";
+import { Role } from "../../generated/prisma/enums";
+import { prisma } from "../../lib/prisma";
+
+const createUser = async (payload: {
+    name: string;
+    email: string;
+    password: string;
+    role: Role;
+}) => {
+    // console.log('create user service')
+
+    const hashedPassword = await bcrypt.hash(payload.password, 10);
+    const result = await prisma.user.create({
+        data: { ...payload, password: hashedPassword },
+    });
+    const {password, ...remainingResult} = result;
+    return remainingResult;
+};
+
 export const AuthService = {
-    // Add service methods here
-    };
+    createUser,
+};
